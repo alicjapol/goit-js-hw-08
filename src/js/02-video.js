7daec19a99eb13b12c0a16ee79b4f5af3131a84b
alicjapol <alicjaxpoltorak@gmail.com>
2023-11-19 19:07:35 +0100

    video

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,37 @@
+import Vimeo from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const player = new Vimeo('vimeo-player');
+
+player.on(
+  'timeupdate',
+  throttle(
+    data => localStorage.setItem('videoplayer-current-time', data.seconds),
+    1000
+  )
+);
+
+const storedTime = localStorage.getItem('videoplayer-current-time');
+
+
+// tego kodu setCurrentTime w ogóle nie rozumiem, skopiowałam z pliku readMe.
+
+
+if (storedTime) {
+  player
+    .setCurrentTime(storedTime)
+    .then(function (seconds) {
+      // seconds = the actual time that the player seeked to
+    })
+    .catch(function (error) {
+      switch (error.name) {
+        case 'RangeError':
+          // the time was less than 0 or greater than the video’s duration
+          break;
+
+        default:
+          // some other error occurred
+          break;
+      }
+    });
+}
