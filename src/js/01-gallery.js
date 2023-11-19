9b7221e1981646bd2ca5b12de6dac52304e809c6
alicjapol <alicjaxpoltorak@gmail.com>
2023-11-19 14:33:42 +0100

    liststyle deleted

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -6,5 +6,35 @@ import "simplelightbox/dist/simple-lightbox.min.css";
 
 import { galleryItems } from './gallery-items';
 // Change code below this line
-console.log('skrrt');
+
+const galleryContainer = document.querySelector(".gallery");
+
+function renderGallery() {
+  galleryItems.forEach((item) => {
+    const galleryItem = document.createElement("li");
+    galleryItem.classList.add("gallery__item");
+
+    const link = document.createElement("a");
+    link.classList.add("gallery__link");
+    link.href = item.original;
+
+    const image = document.createElement("img");
+    image.classList.add("gallery__image");
+    image.src = item.preview;
+    image.alt = item.description;
+
+    link.appendChild(image);
+    galleryItem.appendChild(link);
+    galleryContainer.appendChild(galleryItem);
+  });
+}
+
+renderGallery();
+
+const lightbox = new SimpleLightbox(".gallery a", {
+  captions: true,
+  captionDelay: 250,
+  captionsData: "alt",
+  enableKeyboard: true,
+});
 console.log(galleryItems);
