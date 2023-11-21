e2c85dae052456044e9467fe691aaa0043644c17
alicjapol <alicjaxpoltorak@gmail.com>
2023-11-21 20:28:17 +0100

    saving fixed

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,29 @@
+import throttle from 'lodash.throttle';
 
+const form = document.querySelector('.feedback-form');
+const emailInput = form.querySelector('input[name="email"]');
+const messageInput = form.querySelector('textarea[name="message"]');
+
+const saveState = throttle(() => {
+  localStorage.setItem(
+    'feedback-form-state',
+    JSON.stringify({
+      email: emailInput.value,
+      message: messageInput.value,
+    })
+  );
+}, 500);
+
+form.addEventListener('input', saveState);
+
+form.addEventListener('submit', event => {
+  event.preventDefault();
+
+  localStorage.removeItem('feedback-form-state');
+});
+
+const savedState = JSON.parse(localStorage.getItem('feedback-form-state'));
+if (savedState) {
+  emailInput.value = savedState.email;
+  messageInput.value = savedState.message;
+}
